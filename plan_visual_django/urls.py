6973f5507cdfa3e9e67cdaae674f7d658d421468
seivre <tbg-github@genonline.co.uk>
2023-08-13 14:39:00 +0100

    Re-do of manage swimlanes after losing changes
    Not quite sure what happened but I ended up with the changes for manage swimlanes functionality missing from Master and no commits either.
    
    All related to having two branches at the same time and not fully understanding the implications.

--- plan_visual_django/urls.py
+++ plan_visual_django/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("add-plan", views.add_plan),
     path("re-upload-plan/<int:pk>", views.re_upload_plan),
     path("add-visual/<int:plan_id>", views.add_visual),
+    path("manage-swimlanes-for-visual/<int:visual_id>/", views.manage_swimlanes_for_visual, name="manage-swimlanes"),
     path("edit-visual/<int:visual_id>", views.edit_visual),
     path("manage-plans", views.manage_plans, name="manage_plans"),
     path("delete-plan/<int:pk>/", views.delete_plan, name='delete_plan'),
