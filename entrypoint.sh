41b94f0bd322c1eb462582e3b3cab4d387932ccc
seivre <tbg-github@genonline.co.uk>
2023-10-06 14:50:49 +0100

    PV-165 Implement common colors and style available to all users
    Added makesuperuser command so that an admin user is created when creating a container.  Generates random password and prints it out - a good alternative to trying to store it somewhere secret.
    
    Also added command to createsuperuser and add_common_data to entrypoint.sh which is executed whenever the container is created or updated.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+python manage.py makesuperuser
+python manage.py add_common_data
 
 gunicorn plan_visualiser_2023_02.wsgi:application --bind 0.0.0.0:8000
