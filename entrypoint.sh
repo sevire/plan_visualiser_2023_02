fb1395a71263053f6fb0d61098cd9427a6737f5a
seivre <tbg-github@genonline.co.uk>
2023-10-12 13:30:42 +0100

    PV-183 Pre-loading of common/shared data doesn't reset auto creation of pk
    Change to entrypoint.sh to add command...
    
    python manage.py sqlsequencereset plan_visual_django
    
    which will reset pk sequence to take account of manually specified pks.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,7 @@ python manage.py collectstatic --noinput
 python manage.py makesuperuser
 python manage.py add_common_data
 
+# Reset pk sequence after adding common data with hard-coded pks.
+python manage.py sqlsequencereset plan_visual_django
+
 gunicorn plan_visualiser_2023_02.wsgi:application --bind 0.0.0.0:8000
