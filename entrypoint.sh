7f833a16c1e337261893fa4e5b44afcfd0042530
seivre <tbg-github@genonline.co.uk>
2023-10-17 12:56:26 +0100

    PV-183 Pre-loading of common/shared data doesn't reset auto creation of pk
    Add new command to update the primary key next sequence number for all tables where entries have been added with hard-coded primary keys.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ python manage.py collectstatic --noinput
 python manage.py add_common_data
 
 # Reset pk sequence after adding common data with hard-coded pks.
-python manage.py sqlsequencereset plan_visual_django
+python manage.py update_pk_value
 
 gunicorn plan_visualiser_2023_02.wsgi:application --bind 0.0.0.0:8000
