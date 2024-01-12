c115771e80e61aa717fbef4627f2b27ff0c81a72
Thomas Gaylard <tbg-github@genonline.co.uk>
2024-01-12 13:02:39 +0000

    [PV-249] Docker - automatic restart of containers on build not working.
    Added back in some of the logic I removed when trying to simplify - just so that the output goes to a log file and we can start as a background task.

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,2 +1,18 @@
-docker build --tag "poap-v01" .
-docker run --publish 8000:8000 --detach --name poap poap-v01
+echo "Re-building Docker images..."
+#set -o errexit
+#test -e .env && source .env || echo "$0: warn: no '.env' file in $(pwd): default values will be used"
+#
+readonly TMPDIR="/tmp/git-post-commit"
+test -d $TMPDIR || mkdir -p $TMPDIR
+
+readonly LOGFILE=$(mktemp -q ${TMPDIR}/plan_visualiser-"$(date '+%Y%m%d-%H%M%S')"-XXX.log)
+exec 1>$LOGFILE
+exec 2>&1
+
+echo "Docker compose beginning..."
+docker compose up -d --build
+
+echo "Docker compose complete, pruning beginning..."
+docker system prune -f
+
+echo "Deployment to local Docker environment complete."
