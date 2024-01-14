ebf2470aba0adb9dbcf3e9ac259d36b71f0ecf3c
Thomas Gaylard <tbg-github@genonline.co.uk>
2024-01-14 14:15:58 +0000

    [PV-249][WIP] Docker - automatic restart of containers on build not working.
    Changes to logic in dockerbuild.sh to:
    - move logfile into project structure
    - check last commit message and only rebuild if not [WIP]
    - Run docker compose up in detached mode
    - Restarts containers after build (because if only code change they wouldn't restart normally, but we need them to to pick up code changes in Gunicorn.

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,18 +1,37 @@
-echo "Re-building Docker images..."
-#set -o errexit
-#test -e .env && source .env || echo "$0: warn: no '.env' file in $(pwd): default values will be used"
-#
-readonly TMPDIR="/tmp/git-post-commit"
-test -d $TMPDIR || mkdir -p $TMPDIR
+# Rebuilds all containers and then restarts them.
+# NOTE: At the moment there is no intelligence to work out what might have changed and only rebuild changed things.
+# The approach is to rebuild everything for safety.  Also note that if it's only source code that has changed, I don't
+# believe that triggers a change to the containers so they wouldn't restart just by docker compose up, but at least
+# Gunicorn needs to be restarted after code change to ensure that any caches are destroyed so changes are seen.
+# ToDo: Revisit sophistication of dockerbuild.sh to see whether more intelligence should be built into it.
 
-readonly LOGFILE=$(mktemp -q ${TMPDIR}/plan_visualiser-"$(date '+%Y%m%d-%H%M%S')"-XXX.log)
+# First check whether last commit was WIP commit.  If so then don't do anything.
+LAST_COMMIT_MSG=$(git log -1 --pretty=%B)
+if [[ $LAST_COMMIT_MSG = *'[WIP]'* ]]; then
+    echo "Work in progress commit, not rebuilding"
+    exit 0
+fi
+
+echo "Re-building Docker images..."
+test -e .env && source .env || echo "$0: warn: no '.env' file in $(pwd): default values will be used"
+
+readonly LOG_DIR=$(pwd)/docker-build-logs
+test -d $LOG_DIR || mkdir -p $LOG_DIR
+
+readonly LOGFILE=${LOG_DIR}/plan_visualiser-"$(date '+%Y%m%d-%H%M%S')".log
 exec 1>$LOGFILE
 exec 2>&1
 
+#echo "Taking docker down before rebuild"  # Not sure whether this is necessary but haven't found better way yet.
+#docker compose down
+
 echo "Docker compose beginning..."
-docker compose up -d --build
+docker compose up -d --detach --build
 
 echo "Docker compose complete, pruning beginning..."
 docker system prune -f
+
+echo "Restarting containers after rebuild"
+docker restart $(docker ps -q)
 
 echo "Deployment to local Docker environment complete."
