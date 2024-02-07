074d8d5867bb4c5b134dcefe7524ff45bece85d2
Thomas Gaylard <tbg-github@genonline.co.uk>
2024-02-07 21:25:43 +0000

    [PV-109][WIP] Develop approach for deploying to staging
    (33rd attempt)
    
    Getting there!
    
    Include colour change of headings to check app deployment working (cyanish).
    
    Have created a deploy-staging.yml file to drive the github action, which supersedes running a bash script.  I know it's not complete or correct but want to try pushing to Development branch to see how far it gets.
    
    (NOTE: As I'm not running the deployment script the check for WIP isn't there at the moment.

---
+++ plan_visual_django/deploy_on_remote.sh
@@ -0,0 +1,39 @@
+DO_DOCKER_REGISTRY_NAME=$1
+GITHUB_SHA=$2
+
+cd /docker_app_root/plan_visualiser_2023_02 || exit
+git pull
+
+# Login to registry
+doctl registry login
+docker ps
+
+# Stop running Nginx container and remove old container.
+docker stop plan_visualiser_2023_02-nginx-1
+docker rm plan_visualiser_2023_02-nginx-1
+
+# Stop running Gunicorn container and remove old container.
+docker stop plan_visualiser_2023_02-django_gunicorn-1
+docker rm plan_visualiser_2023_02-django_gunicorn-1
+
+# Stop running Postgres container and remove old container.
+docker stop plan_visualiser_2023_02-db-1
+docker rm plan_visualiser_2023_02-db-1
+
+# Run a new Nginx container from a new image
+docker run -d \
+--restart always \
+--name plan_visualiser_2023_02-nginx-1 \
+"${DO_DOCKER_REGISTRY_NAME}"/plan_visualiser_2023_02-nginx:$(echo $GITHUB_SHA | head -c7)
+
+# Run a new Gunicorn container from a new image
+docker run -d \
+--restart always \
+--name plan_visualiser_2023_02-django_gunicorn-1 \
+"${DO_DOCKER_REGISTRY_NAME}"/plan_visualiser_2023_02-django_gunicorn:$(echo $GITHUB_SHA | head -c7)
+
+# Run a new Postgres container from a new image
+docker run -d \
+--restart always \
+--name plan_visualiser_2023_02-db-1 \
+"${DO_DOCKER_REGISTRY_NAME}"/postgres:$(echo $GITHUB_SHA | head -c7)
