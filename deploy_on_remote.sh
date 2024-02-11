a394dfbe5b6b01c2cca26241b5680ae83f676dc1
Thomas Gaylard <tbg-github@genonline.co.uk>
2024-02-11 13:38:13 +0000

    [PV-109][WIP] Develop approach for deploying to staging
    (41st attempt)
    
    Postgres container is running but volume mapping isn't right- trying to correct that.

--- deploy_on_remote.sh
+++ deploy_on_remote.sh
@@ -39,4 +39,5 @@ docker run -d \
 docker run -d \
 --restart always \
 --name plan_visualiser_2023_02-db-1 \
+--volume data:/var/lib/postgresql/data
 "${DO_DOCKER_REGISTRY_NAME}"/postgres:$(echo $GITHUB_SHA | head -c7)
