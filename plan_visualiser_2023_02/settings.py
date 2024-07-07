b792bb5e8374ed9a8a3fa774e20407dd14b02e9b
Thomas Gaylard <tbg-github@genonline.co.uk>
2024-07-07 16:36:52 +0100

    [PV-260][WIP] Ensure DEBUG flag set appropriately depending upon which environment is running (e.g. local Docker should be True)

--- plan_visualiser_2023_02/settings.py
+++ plan_visualiser_2023_02/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
-import os
+import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,28 +25,32 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # If it is equal to 'production', then we are in production mode.
 # Otherwise, we are in development mode.
 DJANGO_ENVIRONMENT = os.getenv('DJANGO_ENVIRONMENT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 # Read other env variables here (so they are all in one place)
-# ToDo: Gather together all environment variable reads in settings.py
-LOGGING_LEVEL_H_CONSOLE = os.getenv('LOGGING_LEVEL_H_CONSOLE', 'INFO').upper()  # Handler/Console
-LOGGING_LEVEL_H_FILE = os.getenv('LOGGING_LEVEL_H_FILE', 'DEBUG').upper()  # Handler/File
-LOGGING_LEVEL_L_DJANGO = os.getenv('LOGGING_LEVEL_L_DJANGO', 'DEBUG').upper()  # Logger/Django
-LOGGING_LEVEL_L_ROOT = os.getenv('LOGGING_LEVEL_L_ROOT', 'DEBUG').upper()  # Logger/Root
 
 if DJANGO_ENVIRONMENT == 'production':
-    print('Using production environment')
-    # SECURITY WARNING: keep the secret key used in production secret!
+    logging.info('Using production environment')
     SECRET_KEY = os.getenv('SECRET_KEY')
-
-    # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = False
+
+    # Set logging levels from env variable if present but use defaults based on production if not.
+    LOGGING_LEVEL_H_CONSOLE = os.getenv('LOGGING_LEVEL_H_CONSOLE', 'INFO').upper()  # Handler/Console
+    LOGGING_LEVEL_H_FILE = os.getenv('LOGGING_LEVEL_H_FILE', 'INFO').upper()  # Handler/File
+    LOGGING_LEVEL_L_DJANGO = os.getenv('LOGGING_LEVEL_L_DJANGO', 'INFO').upper()  # Logger/Django
+    LOGGING_LEVEL_L_ROOT = os.getenv('LOGGING_LEVEL_L_ROOT', 'INFO').upper()  # Logger/Root
 else:
     print('Using development environment')
     # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = "django-insecure-@!(&2yeohsybrswkzk#75vmj&w5c1l@!xftsbkvuzc+x4z$0yi"
-
-    # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
+
+    # Set logging levels from env variable if present but use defaults based on development if not.
+    LOGGING_LEVEL_H_CONSOLE = os.getenv('LOGGING_LEVEL_H_CONSOLE', 'INFO').upper()  # Handler/Console
+    LOGGING_LEVEL_H_FILE = os.getenv('LOGGING_LEVEL_H_FILE', 'DEBUG').upper()  # Handler/File
+    LOGGING_LEVEL_L_DJANGO = os.getenv('LOGGING_LEVEL_L_DJANGO', 'DEBUG').upper()  # Logger/Django
+    LOGGING_LEVEL_L_ROOT = os.getenv('LOGGING_LEVEL_L_ROOT', 'DEBUG').upper()  # Logger/Root
 
 ALLOWED_HOSTS = ['*']
 
@@ -145,7 +149,7 @@ WSGI_APPLICATION = "plan_visualiser_2023_02.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 if DJANGO_ENVIRONMENT == 'production':
-    # print("Using PostgreSQL database in production")
+    print("Using PostgreSQL database in staging/production")
     # Use the PostgreSQL database in production
     DATABASES = {
         'default': {
@@ -158,7 +162,7 @@ if DJANGO_ENVIRONMENT == 'production':
         }
     }
 else:
-    # print("Using SQLite database in development")
+    print("Using SQLite database in development")
     # Use the SQLite database in development
     DATABASES = {
         "default": {
@@ -219,8 +223,6 @@ LOGIN_REDIRECT_URL = '/'
 # Email settings, mostly to support resetting of passwords for users
 EMAIL_HOST = ""
 EMAIL_PORT = 465
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = "(secret)"
 EMAIL_USE_SSL = True
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
