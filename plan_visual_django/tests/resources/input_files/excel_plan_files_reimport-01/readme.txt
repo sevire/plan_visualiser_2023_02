e5fb776080acf2e4966929846f5134acbce8d06b
Thomas Gaylard <tbg-github@genonline.co.uk>
2024-09-03 21:16:29 +0100

    [PV-232][WIP] Re-visit Upload latest plan functionality

---
+++ plan_visual_django/tests/resources/input_files/excel_plan_files_reimport-01/readme.txt
@@ -0,0 +1,3 @@
+This folder was just created to have similar files to the other re-import files but with the activity_name columns as Task Name rather than Task which is what is expected for unit tests.
+
+Should address this anomaly at some point!
--- plan_visual_django/tests/resources/test_configuration.py
+++ plan_visual_django/tests/resources/test_configuration.py
@@ -11,3 +11,4 @@ test_fixtures_folder = 'resources/test_fixtures'
 # base folder for finding test related files such as fixtures, data files etc.
 test_data_base_folder = os.path.join(settings.BASE_DIR, 'plan_visual_django', 'tests')
 excel_input_files_folder = os.path.join(test_data_base_folder, "resources", "input_files", "excel_plan_files")
+excel_reimported_files_folder = os.path.join(test_data_base_folder, "resources", "input_files", "excel_plan_files_reimport")
