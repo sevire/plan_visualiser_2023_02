9c59c62988304e8942548c9634c6f76e2c32f197
Thomas Gaylard <tbg-github@genonline.co.uk>
2025-03-11 18:39:21 +0000

    still sorting db name and host

--- plan_visualiser_2023_02/settings.py
+++ plan_visualiser_2023_02/settings.py
@@ -198,7 +198,7 @@ if DJANGO_ENVIRONMENT == 'production' or DJANGO_ENVIRONMENT == 'staging':
             'NAME': os.getenv('POSTGRES_NAME'),
             'USER': os.getenv('POSTGRES_USER'),
             'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-            'HOST': os.getenv('POSTGRES_DB', 'db'),
+            'HOST': os.getenv('POSTGRES_DB', 'postgres_pv'),
             'PORT': '5432',
         }
     }
