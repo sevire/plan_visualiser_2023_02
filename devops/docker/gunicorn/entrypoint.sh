1caf90bdfda90dd92fc6c22b69ad90ee6bb858c7
Thomas Gaylard <tbg-github@genonline.co.uk>
2025-05-26 14:57:36 +0100

    [WIP] Keep going some more

--- devops/docker/gunicorn/entrypoint.sh
+++ devops/docker/gunicorn/entrypoint.sh
@@ -2,38 +2,54 @@
 
 echo "
 =================================================================
-||                                                               ||
-||                 ENTRYPOINT.SH STARTING                        ||
-||                 $(date '+%Y-%m-%d %H:%M:%S')                           ||
-||                                                               ||
+||                                                             ||
+||                 ENTRYPOINT.SH STARTING                      ||
+||                 $(date '+%Y-%m-%d %H:%M:%S')                         ||
+||                                                             ||
 =================================================================
 "
 
 echo "Echoing current directory"
 pwd
 
-echo "Applying migrations...\n"
+echo "
+=================================================================
+|| Applying migrations...                                      ||
+=================================================================
+"
+
 python manage.py migrate --noinput
-
-echo "Collecting static..."
-python manage.py collectstatic --noinput
-
-# Add common data and initial users
-echo "Adding common data..."
-python manage.py add_common_data
-
-# Reset pk sequence after adding common data with hard-coded pks.
-echo "Updating primary key sequence after adding common data"
-python manage.py update_pk_value
-
-echo "Starting gunicorn..."
-gunicorn plan_visualiser_2023_02.wsgi:application --bind 0.0.0.0:8000
 
 echo "
 =================================================================
-||                                                               ||
-||                 ENTRYPOINT.SH STARTING GUNICORN (LAST TASK)   ||
-||                 $(date '+%Y-%m-%d %H:%M:%S')                           ||
-||                                                               ||
+|| Collecting static...                                        ||
 =================================================================
 "
+
+python manage.py collectstatic --noinput
+
+# Add common data and initial users
+echo "
+=================================================================
+|| Adding common data...                                       ||
+=================================================================
+"
+python manage.py add_common_data
+
+echo "
+=================================================================
+|| Updating primary key sequence after adding common data...   ||
+=================================================================
+"
+python manage.py update_pk_value
+
+echo "
+=================================================================
+||                                                             ||
+||                 ENTRYPOINT.SH STARTING GUNICORN (LAST TASK) ||
+||                 $(date '+%Y-%m-%d %H:%M:%S')                         ||
+||                                                             ||
+=================================================================
+"
+gunicorn plan_visualiser_2023_02.wsgi:application --bind 0.0.0.0:8000
+
