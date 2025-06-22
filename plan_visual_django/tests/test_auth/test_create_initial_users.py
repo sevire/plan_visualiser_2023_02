0fef8db9582477745914e29cb641475e01c9c780
Thomas Gaylard <tbg-github@genonline.co.uk>
2025-06-22 15:27:27 +0100

    NoJira - create initial users iterating

--- plan_visual_django/tests/test_auth/test_create_initial_users.py
+++ plan_visual_django/tests/test_auth/test_create_initial_users.py
@@ -1,8 +1,7 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from plan_visual_django.services.initialisation.db_initialisation import set_initial_user_data, create_initial_users
-from utilities import set_env_variables
+from plan_visual_django.tests.resources.utilities import set_env_variables
 
 User = get_user_model()
 
