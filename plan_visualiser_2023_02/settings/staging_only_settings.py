a1c69ca96b908f949635762afe272bca2caad5bd
Thomas Gaylard <tbg-github@genonline.co.uk>
2025-06-27 14:23:52 +0100

    NoJira password reset email domain error

--- plan_visualiser_2023_02/settings/staging_only_settings.py
+++ plan_visualiser_2023_02/settings/staging_only_settings.py
@@ -2,5 +2,8 @@ from .base_settings import *
 from .common_staging_production_settings import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1', 'http://138.68.160.214']
+
+# Set default domain for sending password reset emails etc.
+DEFAULT_DOMAIN = "138.68.160.214"
+ALLOWED_HOSTS = [DEFAULT_DOMAIN]
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1', 'http://'+DEFAULT_DOMAIN]
